7e8b8b9f4e4ec85c216aad161917076ca9fbca78
NAGARAJA DEVADIGA <nagaraja.devadiga@espn.com>
2020-07-25 20:42:51 +0530

    fix

--- src/main/java/com/cricketfoursix/cricketdomain/common/league/LeagueInfo.java
+++ src/main/java/com/cricketfoursix/cricketdomain/common/league/LeagueInfo.java
@@ -5,12 +5,43 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LeagueInfo {
+    private long leagueId;
     private String leagueName;
-    private String tournament;
+    private boolean tournament;
+
+    public long getLeagueId() {
+        return leagueId;
+    }
+
+    public void setLeagueId(long leagueId) {
+        this.leagueId = leagueId;
+    }
+
+    private String abbreviation;
+
+    @Override
+    public String toString() {
+        return "LeagueInfo{" +
+                "leagueId=" + leagueId +
+                ", leagueName='" + leagueName + '\'' +
+                ", tournament=" + tournament +
+                ", abbreviation='" + abbreviation + '\'' +
+                ", leagueSeasonMap=" + leagueSeasonMap +
+                '}';
+    }
+
     private Map<Integer, LeagueSeason> leagueSeasonMap = new HashMap<>();
 
     public String getLeagueName() {
         return leagueName;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
     }
 
     public void setLeagueName(String leagueName) {
@@ -26,11 +57,11 @@ public class LeagueInfo {
         this.leagueSeasonMap = leagueSeasonMap;
     }
 
-    public String getTournament() {
+    public boolean isTournament() {
         return tournament;
     }
 
-    public void setTournament(String tournament) {
+    public void setTournament(boolean tournament) {
         this.tournament = tournament;
     }
 }
