16f8e335f2b2ceb90d385700b76423fb43ecb538
NAGARAJA DEVADIGA <nagaraja.devadiga@espn.com>
2020-08-08 12:30:42 +0530

    fix seasons

--- src/main/java/com/cricketfoursix/cricketdomain/common/game/GameInfo.java
+++ src/main/java/com/cricketfoursix/cricketdomain/common/game/GameInfo.java
@@ -14,6 +14,10 @@ public class GameInfo {
     private GameStatus gameStatus;
     private String venue;
     private long competitor1;
+    private String team1Name;
+    private String team2Name;
+    private String team1Score;
+    private String team2Score;
 
     public long getGameId() {
         return gameId;
@@ -146,6 +150,38 @@ public class GameInfo {
 
     public void setNote(String note) {
         this.note = note;
+    }
+
+    public String getTeam1Name() {
+        return team1Name;
+    }
+
+    public void setTeam1Name(String team1Name) {
+        this.team1Name = team1Name;
+    }
+
+    public String getTeam2Name() {
+        return team2Name;
+    }
+
+    public void setTeam2Name(String team2Name) {
+        this.team2Name = team2Name;
+    }
+
+    public String getTeam1Score() {
+        return team1Score;
+    }
+
+    public void setTeam1Score(String team1Score) {
+        this.team1Score = team1Score;
+    }
+
+    public String getTeam2Score() {
+        return team2Score;
+    }
+
+    public void setTeam2Score(String team2Score) {
+        this.team2Score = team2Score;
     }
 
     @Override
